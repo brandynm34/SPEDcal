a298f7c0b84f6303798c6d7f7e5a77726cae062f
Anna Ingels <annaingels@Annas-MacBook-Pro.local>
2017-11-14 13:42:16 -0800

    added dashboard and fixed tabs to route correctly

---
+++ src/pages/dashboard/dashboard.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { TranslateModule } from '@ngx-translate/core';
+import { IonicPageModule } from 'ionic-angular';
+
+import { Dashboard } from './dashboard';
+
+
+@NgModule({
+  declarations: [
+    Dashboard,
+  ],
+  imports: [
+    IonicPageModule.forChild(Dashboard),
+    TranslateModule.forChild()
+  ],
+  exports: [
+    Dashboard
+  ]
+})
+export class DashboardModule { }
---
+++ src/pages/dashboard/dashboard.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController } from 'ionic-angular';
+
+import { ListMasterPage} from '../list-master/list-master';
+
+
+@IonicPage()
+@Component({
+  selector: 'dashboard',
+  templateUrl: 'dashboard.html'
+})
+export class Dashboard {
+
+  constructor(
+    public navCtrl: NavController,
+  ){
+    
+      }
+  
+  openStudents() {
+    this.navCtrl.push('ListMasterPage');
+  }
+
+  
+
+}
