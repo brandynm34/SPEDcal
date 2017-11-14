3cac54c922436de6d4c2b0ef7f0dd33aa6f2a723
brandynm34 <brandynm34@yahoo.com>
2017-11-13 20:08:56 -0800

    Revert "Issues#5/#15"

--- src/pages/signup/signup.ts
+++ src/pages/signup/signup.ts
@@ -14,9 +14,10 @@ export class SignupPage {
   // The account fields for the login form.
   // If you're using the username field with or without email, make
   // sure to add it to the type
-  account: { name: string, email: string, password: string } = {
+  account: { name: string, email: string, user: string, password: string } = {
     name: 'Test Human',
     email: 'test@example.com',
+    user: 'tester',
     password: 'test'
   };
 
