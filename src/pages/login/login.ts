f89b26a5496de4854682b3c822902c97941d41dc
amandaratio <amandaratio@gmail.com>
2018-01-28 10:16:27 -0800

    login test

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -37,7 +37,7 @@ export class LoginPage {
 
   // Attempt to login in through our User service
   doLogin() {
-    this.user.login(this.account).subscribe((resp) => {
+    this.user.googlePlusLogin(this.account).subscribe((resp) => {
       if(resp) {
         console.log(resp);
         this.user.setTeacher(resp);
